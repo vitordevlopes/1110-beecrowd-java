a61d77a2eba15cd5054a6b8b4e8f1e6e66baf882
Vitor <timecarinhasandrade@gmail.com>
2024-11-13 18:18:03 -0300

    last changes

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,50 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> entradas = new ArrayList<>();
 
-        int  qtdeCartas = scanner.nextInt();
+        while (true) {
+            int entrada = scanner.nextInt();
+
+            if (entrada == 0) {
+                break;
+            }
+            entradas.add(entrada);
+        }
+        scanner.close();
 
         ArrayList<Integer> listaCartas = new ArrayList<>();
+        String removed = "";
 
-            for (int i = qtdeCartas; i > 0; i--) {
+        // percorre cada posicao do array entradas para ser colocado no proximo for e descobrir o valor
+        for (int i = 0; i < entradas.size(); i++) {
 
-                listaCartas.add(i);
-
+            for (int qtdeCartas = entradas.get(i); qtdeCartas > 0; qtdeCartas--) {
+                listaCartas.add(qtdeCartas);
             }
 
-            String removed = "";
+            while (listaCartas.size() > 1) {
 
-            while (listaCartas.size() >= 2) {
-
+                // descobre a a posicao da ultima carta
                 int ultimaPosicao = listaCartas.size() - 1;
 
 
+                // remove a carta na ultima posicao e adiciona ela dentro de uma variavel string
+                removed += String.valueOf(listaCartas.remove(ultimaPosicao)) + ", ";
 
-               removed += String.valueOf(listaCartas.remove(ultimaPosicao)) + ", ";
-
-
-               int ultimoElemento = listaCartas.get(listaCartas.size() - 1);
-               listaCartas.remove(listaCartas.size() - 1);
-               listaCartas.add(0, ultimoElemento);
+                // descobre a nova carta que ocupa a ultima posicao
+                int ultimoElemento = listaCartas.get(listaCartas.size() - 1);
+                listaCartas.add(0, ultimoElemento);
+                listaCartas.remove(listaCartas.size() - 1);
 
             }
 
-        System.out.println("Discarded cards: " + removed.substring(0, removed.length() - 2));
-        System.out.println("Remaining card: " + listaCartas.get(0));
+            System.out.println("Discarded cards: " + removed.substring(0, removed.length() - 2));
+            System.out.println("Remaining card: " + listaCartas.get(0));
+            removed = "";
+            listaCartas.clear();
+
+        }
 
     }
 
