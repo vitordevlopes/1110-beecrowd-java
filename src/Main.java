61765be3d2b41eb835c4064860800dcb67a3f525
Vitor <timecarinhasandrade@gmail.com>
2024-11-14 10:55:33 -0300

    Simplify code by using for-each to iterate over the entries list

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,51 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> entradas = new ArrayList<>();
 
-        int  qtdeCartas = scanner.nextInt();
+        while (true) {
+            int entrada = scanner.nextInt();
 
+            if (entrada == 0) {
+                break;
+            }
+            entradas.add(entrada);
+        }
+        scanner.close();
+
+        //o array listaCartas vai guardar a quantidade de cartas digitada pelo usuario
         ArrayList<Integer> listaCartas = new ArrayList<>();
+        String cartasRemovidas = "";
 
-            for (int i = qtdeCartas; i > 0; i--) {
+        // percorre cada posicao do array entradas para ser colocado no proximo for e descobrir o valor
+        for (Integer quantidadeCartas : entradas) {
 
-                listaCartas.add(i);
-
+            for (int qtdeCartas = quantidadeCartas; qtdeCartas > 0; qtdeCartas--) {
+                listaCartas.add(qtdeCartas);
             }
 
-            String removed = "";
+            while (listaCartas.size() > 1) {
 
-            while (listaCartas.size() >= 2) {
-
+                // descobre a a posicao da ultima carta
                 int ultimaPosicao = listaCartas.size() - 1;
 
 
+                // remove a carta na ultima posicao e adiciona ela dentro de uma variavel string
+                cartasRemovidas += String.valueOf(listaCartas.remove(ultimaPosicao)) + ", ";
 
-               removed += String.valueOf(listaCartas.remove(ultimaPosicao)) + ", ";
-
-
-               int ultimoElemento = listaCartas.get(listaCartas.size() - 1);
-               listaCartas.remove(listaCartas.size() - 1);
-               listaCartas.add(0, ultimoElemento);
+                // descobre a nova carta que ocupa a ultima posicao
+                int ultimoElemento = listaCartas.get(listaCartas.size() - 1);
+                listaCartas.add(0, ultimoElemento);
+                listaCartas.remove(listaCartas.size() - 1);
 
             }
 
-        System.out.println("Discarded cards: " + removed.substring(0, removed.length() - 2));
-        System.out.println("Remaining card: " + listaCartas.get(0));
+            System.out.println("Discarded cards: " + cartasRemovidas.substring(0, cartasRemovidas.length() - 2));
+            System.out.println("Remaining card: " + listaCartas.get(0));
+            cartasRemovidas = "";
+            listaCartas.clear();
+
+        }
 
     }
 
